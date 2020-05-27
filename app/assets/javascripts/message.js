f4f253943e196cfe8e8de256dc84f09e87d45017
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-05-27 20:33:19 +0900

    Update messages

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `<div class="message">
+          <div class="message__box">
+            <div class="message__box__name">
+              ${message.user_name}
+            </div>
+            <div class="message__box__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="message__text__content">
+              ${message.content}
+            </p>
+            <img src=${message.image} >
+          </div>
+        </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="message">
+          <div class="message__box">
+            <div class="message__box__name">
+              ${message.user_name}
+            </div>
+            <div class="message__box__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="message__text__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+        return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.message-list').append(html);
+       $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+       $('form')[0].reset();
+       $('.submit-btn').prop('disabled', false)
+     })
+     .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+  });
+});
